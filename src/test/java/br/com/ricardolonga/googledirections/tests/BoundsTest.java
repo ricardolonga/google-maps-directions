7d05ad5247d1c205046957e897cf6f390ce597da
ricardo.longa <ricardo.longa@localhost.localdomain>
2013-10-01 13:34:34 -0300

    Retornando os testes da classe BoundsTest.java.

--- src/test/java/br/com/ricardolonga/googledirections/tests/BoundsTest.java
+++ src/test/java/br/com/ricardolonga/googledirections/tests/BoundsTest.java
@@ -51,30 +51,29 @@ public class BoundsTest {
 
     @Test
     public void the_route_must_have_bounds() throws GoogleDirectionsException {
-        // DirectionsResponse response =
-        // directionsSearch.create().from(AddressesHelper.FLORIANOPOLIS).to(AddressesHelper.ARARANGUA).go();
-        // Assert.assertNotNull(response.getRoutes().get(0).getBounds());
+        DirectionsResponse response = directionsSearch.create().from(AddressesHelper.FLORIANOPOLIS).to(AddressesHelper.ARARANGUA).go();
+        Assert.assertNotNull(response.getRoutes().get(0).getBounds());
     }
 
-    // @Test
+    @Test
     public void the_northeast_bounds_should_have_latitude() throws GoogleDirectionsException {
         DirectionsResponse response = directionsSearch.create().from(AddressesHelper.FLORIANOPOLIS).to(AddressesHelper.ARARANGUA).go();
         Assert.assertEquals(Double.valueOf(-27.5846237), response.getRoutes().get(0).getBounds().getNortheast().getLatitude());
     }
 
-    // @Test
+    @Test
     public void the_northeast_bounds_should_have_longitude() throws GoogleDirectionsException {
         DirectionsResponse response = directionsSearch.create().from(AddressesHelper.FLORIANOPOLIS).to(AddressesHelper.ARARANGUA).go();
         Assert.assertEquals(Double.valueOf(-48.54945439999999), response.getRoutes().get(0).getBounds().getNortheast().getLongitude());
     }
 
-    // @Test
+    @Test
     public void the_southwest_bounds_should_have_latitude() throws GoogleDirectionsException {
         DirectionsResponse response = directionsSearch.create().from(AddressesHelper.FLORIANOPOLIS).to(AddressesHelper.ARARANGUA).go();
         Assert.assertEquals(Double.valueOf(-28.9397731), response.getRoutes().get(0).getBounds().getSouthwest().getLatitude());
     }
 
-    // @Test
+    @Test
     public void the_southwest_bounds_should_have_longitude() throws GoogleDirectionsException {
         DirectionsResponse response = directionsSearch.create().from(AddressesHelper.FLORIANOPOLIS).to(AddressesHelper.ARARANGUA).go();
         Assert.assertEquals(Double.valueOf(-49.4977452), response.getRoutes().get(0).getBounds().getSouthwest().getLongitude());
